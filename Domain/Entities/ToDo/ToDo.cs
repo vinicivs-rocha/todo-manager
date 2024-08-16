a0cf043a69abea23cbd41150ba0d076792393b19
vinicivs-rocha <viniciusroch65@gmail.com>
2024-08-16 19:00:45 -0300

    compose value objects creation

--- Domain/Entities/ToDo/ToDo.cs
+++ Domain/Entities/ToDo/ToDo.cs
@@ -8,7 +8,7 @@ namespace Domain.Entities.ToDo;
 public class ToDo
 {
     public Guid Id { get; private init; } = Guid.NewGuid();
-    public required string Title { get;  init; }
+    public required string Title { get; init; }
     public required string Description { get; init; }
     public required TaskPriority Priority { get; init; }
     public required DateTime DueDate { get; init; }
@@ -16,22 +16,16 @@ public class ToDo
 
     public static Either<DomainError, ToDo> Create(ToDoCreationData data)
     {
-        var priority = TaskPriority.FromInt(data.Priority);
-        if (priority.IsLeft)
-            return priority.Left;
-        
-        var status = TaskStatus.FromInt(data.Status);
-        if (status.IsLeft)
-            return status.Left;
-
-        return new ToDo
-        {
-            Id = data.Id is not null ? Guid.Parse(data.Id) : Guid.NewGuid(),
-            Title = data.Title,
-            Description = data.Description,
-            Priority = priority.Right,
-            DueDate = DateTime.Parse(data.DueDate),
-            Status = status.Right
-        };
+        return TaskPriority.FromInt(data.Priority).FlatMap(priority =>
+            TaskStatus.FromInt(data.Status).Map(status => new ToDo
+            {
+                Id = data.Id is not null ? Guid.Parse(data.Id) : Guid.NewGuid(),
+                Title = data.Title,
+                Description = data.Description,
+                Priority = priority,
+                DueDate = DateTime.Parse(data.DueDate),
+                Status = status
+            })
+        );
     }
 }
