96dd9299363afdd9d49dc98a00fe39beef6387bc
vinicivs-rocha <viniciusroch65@gmail.com>
2024-08-16 20:33:26 -0300

    fix todo creation data fields

--- Domain/Entities/ToDo/ToDoCreationData.cs
+++ Domain/Entities/ToDo/ToDoCreationData.cs
@@ -1,3 +1,16 @@
 namespace Domain.Entities.ToDo;
 
-public record ToDoCreationData(string? Id, string Title, string Description, int Priority, string DueDate, int Status);
+public record ToDoCreationData()
+{
+    public string? Id {get; init;}
+    
+    public required string Title {get; init;}
+    
+    public required string Description {get; init;}
+    
+    public required int Priority {get; init;}
+    
+    public required string DueDate {get; init;}
+    
+    public required int Status {get; init;}
+}
