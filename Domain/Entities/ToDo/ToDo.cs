204393e01925e3ac0aa29c7928d972d88d698d37
vinicivs-rocha <viniciusroch65@gmail.com>
2024-08-16 20:13:28 -0300

    refactor todo creation

--- Domain/Entities/ToDo/ToDo.cs
+++ Domain/Entities/ToDo/ToDo.cs
@@ -1,4 +1,6 @@
+using System.Globalization;
 using Core.Errors;
+using Core.ExtensionMethods;
 using Core.Monads;
 using Domain.ValueObjects;
 using TaskStatus = Domain.ValueObjects.TaskStatus;
@@ -7,25 +9,25 @@ namespace Domain.Entities.ToDo;
 
 public class ToDo
 {
-    public Guid Id { get; private init; } = Guid.NewGuid();
+    public Guid Id { get; init; } = Guid.NewGuid();
     public required string Title { get; init; }
     public required string Description { get; init; }
     public required TaskPriority Priority { get; init; }
     public required DateTime DueDate { get; init; }
     public required TaskStatus Status { get; init; }
 
-    public static Either<DomainError, ToDo> Create(ToDoCreationData data)
+    public static Either<DomainError, ToDo> Create(ToDoCreationData toDoCreationData)
     {
-        return TaskPriority.FromInt(data.Priority).FlatMap(priority =>
-            TaskStatus.FromInt(data.Status).Map(status => new ToDo
-            {
-                Id = data.Id is not null ? Guid.Parse(data.Id) : Guid.NewGuid(),
-                Title = data.Title,
-                Description = data.Description,
-                Priority = priority,
-                DueDate = DateTime.Parse(data.DueDate),
-                Status = status
-            })
+        return TaskPriority.FromInt(toDoCreationData.Priority).FlatMap(priority =>
+            TaskStatus.FromInt(toDoCreationData.Status).FlatMap(status =>
+                toDoCreationData.DueDate.SafeParseToDateTime().Map(dueDate => new ToDo
+                {
+                    Title = toDoCreationData.Title,
+                    Description = toDoCreationData.Description,
+                    Priority = priority,
+                    DueDate = dueDate,
+                    Status = status
+                }))
         );
     }
 }
