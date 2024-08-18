0e7b9437b8294422ca782bfd0ef1cdde3674a998
vinicivs-rocha <viniciusroch65@gmail.com>
2024-08-18 09:19:17 -0300

    refine either value verifier

--- Core/Monads/Either.cs
+++ Core/Monads/Either.cs
@@ -6,10 +6,10 @@ public sealed record Either<TLeft, TRight>(TLeft? Left, TRight? Right)
 {
     [MemberNotNullWhen(true, nameof(Left))]
     [MemberNotNullWhen(false, nameof(Right))]
-    public bool IsLeft => Left is not null && Right is null;
+    public bool IsLeft => Left is not null && EqualityComparer<TRight>.Default.Equals(Right, default);
     [MemberNotNullWhen(true, nameof(Right))]
     [MemberNotNullWhen(false, nameof(Left))]
-    public bool IsRight => Right is not null && Left is null;
+    public bool IsRight => Right is not null && EqualityComparer<TLeft>.Default.Equals(Left, default);
 
     public static implicit operator Either<TLeft, TRight>(TLeft left) => new(left, default);
     public static implicit operator Either<TLeft, TRight>(TRight right) => new(default, right);
