11014c4357c137d2e7bbbd1f13a735a1d3ee1331
vinicivs-rocha <viniciusroch65@gmail.com>
2024-08-18 09:19:38 -0300

    provide dependencies

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,3 +1,7 @@
+using Application.Repositories;
+using Application.UseCases.CreateToDo;
+using Infra.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +12,12 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+// Inject Use Cases
+builder.Services.AddScoped<CreateToDo>();
+
+// Inject Repositories
+builder.Services.AddSingleton<IToDoRepository>(new InMemoryToDoRepository());
 
 var app = builder.Build();
 
